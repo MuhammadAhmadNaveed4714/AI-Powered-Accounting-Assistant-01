e70e946721771b9ed7694cd3828e73b84d09cc21
Muhammad Ahmad Naveed <mynameisahmad4714@gmail.com>
2026-08-19 23:35:02 +0500

    Optimize EasyOCR with lazy loading

--- backend/ai/ocr.py
+++ backend/ai/ocr.py
@@ -1,43 +1,40 @@
 import easyocr
 from ai.ocr_cleaner import clean_ocr_text
 
-# Load EasyOCR Reader
-reader = easyocr.Reader(["en"], gpu=False)
+reader = None
+
+
+def get_reader():
+    global reader
+
+    if reader is None:
+        print("Initializing EasyOCR...")
+        reader = easyocr.Reader(["en"], gpu=False)
+
+    return reader
 
 
 def extract_text_from_image(image_path):
-    """
-    Extract text from an image using EasyOCR.
-
-    Returns:
-        original_text (str): Raw OCR output
-        cleaned_text (str): OCR text after cleaning
-    """
 
     try:
+        reader = get_reader()
 
-        # OCR Extraction
-        results = reader.readtext(
-            image_path,
-            detail=0
+        result = reader.readtext(image_path)
+
+        original_text = " ".join(
+            [item[1] for item in result]
         )
-
-        original_text = "\n".join(results)
 
         print("\n===== ORIGINAL OCR =====\n")
         print(original_text)
 
-        # Clean OCR Output
         cleaned_text = clean_ocr_text(original_text)
 
         print("\n===== CLEANED OCR =====\n")
         print(cleaned_text)
 
-        # Return BOTH versions
         return original_text, cleaned_text
 
     except Exception as e:
-
         print(f"\nOCR Error: {e}")
-
         return "", ""
